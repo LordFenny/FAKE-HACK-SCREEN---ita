a8abc7c1293fa3aae47af00bfc2eaa54c0b14e83
LordFenny <87877442+LordFenny@users.noreply.github.com>
2021-09-27 15:29:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+import time
+from typing import ChainMap
+
+print("                         <<<BENVENUTO>>>                            ")
+print("--------------------------------------------------------------------")
+
+time.sleep(5)
+input("IMMETTERE NOME E COGNOME - ")
+
+input("IMMETTERE PASSWORD - ")
+
+from tqdm import tqdm
+from time import sleep
+
+
+for i in tqdm(range(0, 3), desc =""):
+	sleep(1)
+
+print("account collegato con successo")
+
+print("--------------------------------------------------------------------")
+
+time.sleep(2)
+input("immettere nome e cognome intestatario - ")
+
+time.sleep(1)
+print(".")
+
+time.sleep(1)
+print("..")
+
+time.sleep(1)
+print("...")
+
+time.sleep(1)
+print(".")
+
+time.sleep(1)
+print("..")
+
+time.sleep(1)
+print("...")
+
+print("computer collegato con successo")
+
+print("--------------------------------------------------------------------")
+
+time.sleep(2)
+print("--> iniziando hackeraggio <--")
+
+from tqdm import tqdm
+from time import sleep
+
+
+for i in tqdm(range(0, 5), desc =""):
+	sleep(1)
+
+from tqdm import tqdm
+from time import sleep
+
+
+for i in tqdm(range(0, 5), desc =""):
+	sleep(1)
+
+from tqdm import tqdm
+from time import sleep
+
+
+for i in tqdm(range(0, 5), desc =""):
+	sleep(1)
+
+time.sleep(1)
+print("caricamento completato")
+
+
+time.sleep(5)
+print("10000000" *1000000)
+
+print("")
+print("")
+print("--------------------------------------------------------------------")
+print("--------------------------------------------------------------------")
+print("--------------------------------------------------------------------")
+print("")
+
+time.sleep(3)
+print("<<hackerato con successo --> computer collegato con successo>>")
+
+time.sleep(10)
+
+exit
